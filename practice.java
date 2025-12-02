eeb6ccfff874d9fc3100044e4701722ff9984f75
kashish-syal <kashishsyal32@gmail.com>
2025-12-02 17:29:13 +0530

    initial Commit

---
+++ practice.java
@@ -0,0 +1,8 @@
+// My Bestfriend Essay:
+
+I first met Gurseerat in fourth class! At first I thought she was rude but then after meeting 
+her and talking to her made me realise she is so kind and sweet... She was the first ever Transfer 
+student in our class this thing also made me think that i will never be such good friends with her 
+but destinies have some other plans ... This friendship is about of 11years now still we talk like
+old times even if we are in long distance friendship .. she understands me so well that i just need
+her sometimes ... At last i wanna say have a true /real bestie in your life is a wholesome blessing! 
